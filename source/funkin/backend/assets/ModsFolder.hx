0de33a6547198358f5d2808ee9f151de139e8e92
Frakist <90983918+Frakist@users.noreply.github.com>
2023-05-01 20:00:35 +0300

    fixed hitbox input script

--- source/funkin/backend/assets/ModsFolder.hx
+++ source/funkin/backend/assets/ModsFolder.hx
@@ -59,29 +59,32 @@ class ModsFolder {
 		    FileSystem.createDirectory(addonsPath + "android controls/data/charts/");
 		    sys.io.File.saveContent(addonsPath + "android controls/data/charts/4screenboxes.hx",
 			"import flixel.ui.FlxSpriteButton;
-            import openfl.geom.Rectangle;
-
-            function create() {
-            	var width = FlxG.width / 4;
-            	for (i in 0...4) {
-            		var button = new FlxSpriteButton(width * i).makeGraphic(width, FlxG.height, [0xFFc24b99, 0xFF00ffff, 0xFF12fa05, 0xFFf9393f][i]);
-            		button.width = width;
-            		button.height = FlxG.height;
-            		button.pixels.fillRect(new Rectangle(10, 10, width - 20, FlxG.height - 20), 0x00000000);
-            		add(button);
-            		button.cameras = [camHUD];
-            		button.alpha = 0.5;
-            		var k = FlxG.keys.getKey([37, 40, 38, 39][i]);
-            		button.onDown.callback = function() {
-            			k.press();
-            			button.alpha = 1;
-            		}
-            		button.onUp.callback = function() {
-            			k.release();
-            			button.alpha = 0.5;
-            		}
-            	}
-            }");
+			import openfl.geom.Rectangle;
+			
+			function postCreate() {
+				var camHitboxes = new FlxCamera();
+				FlxG.cameras.add(camHitboxes, false);
+				camHitboxes.bgColor = 0;
+				var width = FlxG.width / 4;
+				for (i in 0...4) {
+					var button = new FlxSpriteButton(width * i).makeGraphic(width, FlxG.height, [0xFFc24b99, 0xFF00ffff, 0xFF12fa05, 0xFFf9393f][i]);
+					button.width = width;
+					button.height = FlxG.height;
+					button.pixels.fillRect(new Rectangle(10, 10, width - 20, FlxG.height - 20), 0x00000000);
+					add(button);
+					button.cameras = [camHitboxes];
+					button.alpha = 0.5;
+					var k = FlxG.keys.getKey([37, 40, 38, 39][i]);
+					button.onDown.callback = function() {
+						k.press();
+						button.alpha = 1;
+					}
+					button.onUp.callback = button.onOut.callback = function() {
+						k.release();
+						button.alpha = 0.5;
+					}
+				}
+			}");
 		}
 		#end
 	}
